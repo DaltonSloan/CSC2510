a47139a44bce8d341e6b575bdeededb26df72cc9
DaltonSloan <157657091+DaltonSloan@users.noreply.github.com>
2024-01-30 00:59:40 -0600

    Update dailygrind.java

--- poetry/dailygrind.java
+++ poetry/dailygrind.java
@@ -1,14 +1,26 @@
 boolean its_time_to_go_home = false;
 boolean away_the_hours = true;
 
-while (away_the_hours) {
-  boolean away_the_hours = true;
+while (away_the_hours)
+{
+    boolean away_the_hours = true;
 
-  Date now = new Date();
-  its_time_to_go_home = now.getHours() > 17
-  && now.getMinutes() > 30;
+    Date now = new Date();
+    its_time_to_go_home = now.getHours() > 17
+    && now.getMinutes() > 30;
 
-  	if (its_time_to_go_home) {
-            break;
-      }
+    if (its_time_to_go_home) 
+    {
+        break;
+    }
 }
+
+try
+    {
+        Thread.sleep(60000);
+    } 
+
+    catch (InterruptedException e) 
+    {
+       // ignore
+    }
